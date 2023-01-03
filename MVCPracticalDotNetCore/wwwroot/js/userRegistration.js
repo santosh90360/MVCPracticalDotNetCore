09e818117660ff54d9b4df6b164a97868606f89c
Santosh Singh <santosh.singh@publicissapient.com>
2023-01-03 14:54:42 +0530

    Added alert

--- MVCPracticalDotNetCore/wwwroot/js/userRegistration.js
+++ MVCPracticalDotNetCore/wwwroot/js/userRegistration.js
@@ -5,11 +5,11 @@
     var email = $('#Email').val();
     var password = $('#Password').val();   
     var gender = $("input[name='Gender']:checked").val();
-    
-    $('#txtFirstNameView').val(firstName);
-    $('#txtLastNameView').val(lastName);
-    $('#txtEmailView').val(email);
-    $('#txtPasswordView').val(password);
-    $('#spGenderView').html(gender);
-    $("#addUserModal").modal('show');
+    alert('First Name: ' + firstName + '\nLast Name: ' + lastName + '\nEmail: ' + email + '\nPassword: ' + password + '\nGender: ' +gender);
+    //$('#txtFirstNameView').val(firstName);
+    //$('#txtLastNameView').val(lastName);
+    //$('#txtEmailView').val(email);
+    //$('#txtPasswordView').val(password);
+    //$('#spGenderView').html(gender);
+    //$("#addUserModal").modal('show');
 }
